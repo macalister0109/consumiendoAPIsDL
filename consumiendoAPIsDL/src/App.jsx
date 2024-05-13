5575843ef98ed5c12c40f5bca50a3dc90e0ab3c3
macalister0109 <macarena.jamett@usach.cl>
2024-05-13 15:43:53 -0400

    act 3

--- consumiendoAPIsDL/src/App.jsx
+++ consumiendoAPIsDL/src/App.jsx
@@ -3,15 +3,13 @@ import './App.css'
 import Feriados from './components/Feriados'
 
 function App() {
-  const [search, setSearch] = useState(0)
+  
 
   return (
     <>
       <h1>Cuanto Farta Pal 18 Cabros</h1>
-      <input type="text" value={search} onChange={(e)=>
-      setSearch(e.target.value)
-    }/>
-      <Feriados search={search} />
+      
+      <Feriados  />
       
       
     </>
--- consumiendoAPIsDL/src/components/Feriados.jsx
+++ consumiendoAPIsDL/src/components/Feriados.jsx
@@ -1,45 +1,76 @@
-import { useState, useEffect } from "react";
-const Feriados = ({search}) => {
-    const [fechasAPI, setFechasAPI] = useState([]);
+
+import  { useState, useEffect } from 'react';
+
+const Feriados = () => {
+    const [fechasFeriado, setFechasFeriado] = useState([]);    
     const url ="https://api.boostr.cl/feriados/en.json";
-    const traerFechas = async () => {
+    const traerFeriados = async()=>{
         try{
             const data = await fetch(url);
             const resultado = await data.json();
             const fechas = resultado.data;
-            setFechasAPI(fechas);
+            setFechasFeriado(fechas);
             console.log(fechas);
+
         }catch(error){
-            console.log("No trae los datos");
-    };
-    useEffect(() => {
-        traerFechas();
-    }, []);
-    let fechasAMostrar = [];
-    if(search === 0){
-        fechasAMostrar = fechasAPI;
-    }else{
-        fechasAMostrar = fechasAPI.filter((fecha) => fecha.date.includes(search));
+            console.log("no trae los datos")
+        }
     }
-    return(
-        <>
-            <div>
-                {
-                    fechasAMostrar.map((fecha) => (
-                        <div key={fecha.date}>
-                            <h2>{fecha.date}</h2>
-                            <p>{fecha.title}</p>
-                        </div>
-                    ))
-                }
-            </div>
-            {fechasAMostrar.length == 0? <p>No hay feriados</p>: null}
-        
-       
-        </>
+    traerFeriados();
 
-        )
-    };
-};
+    const fechaTarget = '2024-09-18';
+    if(traerFeriados.length>0){
+        for(i=0; i<traerFeriados.length; i++){
+            if(traerFeriados.Date===fechaTarget){
+                var fechaFeriadoAEvaluar = traerFeriados.date;
+                var tituloFeriado = traerFeriados.title;
+                var tipoFeriado = traerFeriados.type;
+                var inalienable = traerFeriados.inalienable;
+            }
+        }
+    }
+    
+    const calcularTiempoRestante = (fechaFeriadoAEvaluar)=>{
+        const diferenciaTiempo = +fechaFeriadoAEvaluar - new Date();
+        let tiempoRestante = {}
+        if(diferenciaTiempo>0){
+            tiempoRestante={
+                dias: Math.floor(diferenciaTiempo / (1000 * 60 * 60 * 24)),
+                horas: Math.floor((diferenciaTiempo / (1000 * 60 * 60)) % 24),
+                minutos: Math.floor((diferenciaTiempo / 1000 / 60) % 60),
+                segundos: Math.floor((diferenciaTiempo / 1000) % 60)
+             }        
+         }
+        return tiempoRestante;  
+    } 
 
-    export default Feriados;
+    const [tiempoRestante, setTiempoRestante]=useState(calcularTiempoRestante(fechaFeriadoAEvaluar));
+
+    useEffect(()=>{
+        const timer = setTimeout(()=>{
+            setTiempoRestante(calcularTiempoRestante(fechaFeriadoAEvaluar));
+        },1000);
+        return ()=> clearTimeout(timer);
+    },[tiempoRestante, fechaFeriadoAEvaluar]);
+    
+    return (
+        <div>
+          <h2>Y nos quedan:</h2>
+          <div>
+            {Object.keys(tiempoRestante).length && (
+              <>
+                <p>{tiempoRestante.dias} days</p>
+                <p>{tiempoRestante.horas} hours</p>
+                <p>{tiempoRestante.minutos} minutes</p>
+                <p>{tiempoRestante.segundos} seconds</p>
+              </>
+            )}
+          </div>
+        </div>
+      );
+}
+
+
+export default Feriados;
+
+    
